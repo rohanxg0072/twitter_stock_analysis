cb2beb8344cf6afde99f6abeb9ca1db2436cacd4
Rohan Gupta <rohang0072@gmail.com>
2021-08-01 22:01:46 -0400

    undoing quickchart test

--- flasksite/twitter/routes.py
+++ flasksite/twitter/routes.py
@@ -12,7 +12,6 @@ def twitter_chart():
 
     form = StockForm()
     if form.validate_on_submit():
-        '''
         sentiments_times = get_sentiments_times(form.stock.data)   
         prices = get_prices(sentiments_times[1], (form.stock.data))
         dates = []
@@ -20,9 +19,7 @@ def twitter_chart():
                 dates.append(time[0:10])
 
         sentiments = sentiments_times[0]
-        '''
-        # chart_url = get_chart_url(dates, prices, sentiments, form.stock.data)
-        chart_url = get_chart_url([1, 2, 3], [1, 2, 3], [1, 2, 3], form.stock.data)
+        chart_url = get_chart_url(dates, prices, sentiments, form.stock.data)
         return render_template("twitter.html", form = form, chart_url = chart_url) 
 
     return render_template("twitter.html", form = form)
