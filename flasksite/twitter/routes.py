0fee28f5114fd17b3e44a1798a67187c1ae4893f
Rohan Gupta <rohang0072@gmail.com>
2021-08-01 21:04:26 -0400

    testing quickcharts api

--- flasksite/twitter/routes.py
+++ flasksite/twitter/routes.py
@@ -19,7 +19,8 @@ def twitter_chart():
                 dates.append(time[0:10])
 
         sentiments = sentiments_times[0]
-        chart_url = get_chart_url(dates, prices, sentiments, form.stock.data)
+        # chart_url = get_chart_url(dates, prices, sentiments, form.stock.data)
+        chart_url = "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
         return render_template("twitter.html", form = form, chart_url = chart_url) 
 
     return render_template("twitter.html", form = form)
